d6e2a45e8f1565e210ac9816f3c3f238cea9ffe6
MeshackYaro <yaromeshack@gmail.com>
2024-11-14 09:37:26 +0100

    refactor modal

--- componentt/chart/chartModal.tsx
+++ componentt/chart/chartModal.tsx
@@ -9,7 +9,7 @@ export default function Modal(props:{isOpen:boolean,setOpen: (open: boolean) => 
       };
     
     return (
-        <dialog className={`${props.isOpen? 'flex flex-col': 'hidden'} fixed ${styles.modal}`}>
+        <div className={`${props.isOpen? 'flex flex-col': 'hidden'} fixed t-[50%] l-[50%] inset-0 -translate-[50%_50%] ${styles.modal}`}>
             <p>Register a transaction</p>
             <div>
                 <input type="text" placeholder="Title"/>
@@ -17,9 +17,9 @@ export default function Modal(props:{isOpen:boolean,setOpen: (open: boolean) => 
                 <input type="text" placeholder="Enter description" />
             </div>
             <div className='flex gap-[15px] justify-center items-center'>
-            <button onClick={handleCancel}>Cancel</button>
-            <button onClick={handleCreate}>Create</button>
+                <button onClick={handleCancel}>Cancel</button>
+                <button onClick={handleCreate}>Create</button>
             </div>
-        </dialog>
+        </div>
     )
 }
